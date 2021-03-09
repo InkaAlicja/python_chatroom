f0ce3dbb51205bd26008a9c65371b802f1b9aef5
Inka Sokolowska <inka.sokolowska@student.uj.edu.pl>
2021-03-09 18:19:25 +0100

    Add implementation

---
+++ client.py
@@ -0,0 +1,181 @@
+import socket
+import sys
+import threading
+import traceback
+from signal import signal, SIGINT
+
+
+HOST = 'localhost'
+PORT = 50007
+clients = dict()
+messages = list()
+clients_lock = threading.Lock()
+messages_lock = threading.Lock()
+
+def handler(signal_received, frame):
+    print("SIGINT")
+    for client in clients:
+        try:
+            clients[client][0].close()
+        except:
+            pass
+    try:
+        server_sock.close()
+    except:
+        pass
+
+    exit(0)
+
+def addClient(name, client_sock):
+    for client in clients:
+        try:
+            clients[client][0].sendall(bytes('1', 'utf-8'))
+            clients[client][0].sendall(bytes(str(len(name))+" ", 'utf-8'))
+            clients[client][0].sendall(bytes(name, 'utf-8'))
+
+            client_sock.sendall(bytes('1', 'utf-8'))
+            client_sock.sendall(bytes(str(len(client))+" ", 'utf-8'))
+            client_sock.sendall(bytes(client, 'utf-8'))
+        except:
+            print("cannot send m8s add")
+
+
+def removeClient(name):
+    for client in clients:
+        if not client == name:
+            try:
+                clients[client][0].sendall(bytes("2", 'utf-8'))
+                clients[client][0].sendall(bytes(str(len(name))+" ", 'utf-8'))
+                clients[client][0].sendall(bytes(name, 'utf-8'))
+            except:
+                print("cannot send m8s remove")
+
+
+def quickRemove(name):
+    with clients_lock:
+        clients[name][0].close()
+        removeClient(name)
+        clients.pop(name)
+
+
+def readClient(name):
+    clients[name][0].settimeout(3.0)
+    while True:
+        try:
+            size = ""
+            temp = ""
+            while not temp == " ":
+                size += temp
+                try:
+                    temp = clients[name][0].recv(1)
+                except socket.timeout:
+                    print("client didnt reply, remove "+name)
+                    quickRemove(name)
+                    return
+                temp = temp.decode()
+                if temp == "":  #socket zakonczyl dzialanie
+                    print("remove interrupted "+name)
+                    quickRemove(name)
+                    return
+                if temp == "c": #connection check
+                    temp = ""
+                    clients[name][0].sendall(bytes("3", 'utf-8'))
+
+            size = int(size)
+            if size == 0:   #zostalismy poinformowani ze nastapi odlaczenie
+                break
+            data = clients[name][0].recv(size)
+        except:     #nagle odlaczenie przez blad
+            print("nagle odlaczenie "+name)
+            break
+        if not data:
+            break
+        msg = data.decode()
+        with messages_lock:
+            messages.append((name, msg))
+        print(msg)
+    #cleanup
+    print("remove "+name)
+    quickRemove(name)
+
+#0 to znaczy ze wiadomosc, 1 to znaczy ze dodaj mate, 2 znaczy usun mate
+
+
+def sendMsg(receiver, msg):
+    try:
+        clients[receiver][0].sendall(bytes("0", 'utf-8'))
+        clients[receiver][0].sendall(bytes(str(len(msg))+" ", 'utf-8'))
+        clients[receiver][0].sendall(bytes(msg, 'utf-8'))
+    except:
+        print("cannot send in msgHandler")
+
+
+
+def msgHandler(sender, msg):
+    parts = msg.splitlines()
+    receiver = parts[0]
+    str = sender + " => " + receiver + ":" + msg[len(receiver):]
+    print(str)
+
+    if parts[0] == 'ALL':
+        for client in clients:
+            sendMsg(client, str)
+    else:
+        sendMsg(receiver, str)
+        sendMsg(sender, str)
+
+
+def Timer():
+    while True:
+        if not messages:
+            continue
+        with messages_lock:
+            with clients_lock:
+                for sender, msg in messages:
+                    msgHandler(sender, msg) #inaczej moga sie zdazyc wyczyscic zanim skonczymy
+                                            # a i tak potrzebuja tych lockow
+            messages.clear()
+
+
+timer = threading.Thread(target=Timer)
+timer.daemon = True
+timer.start()
+
+signal(SIGINT, handler)
+
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
+        server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_sock.bind((HOST, PORT))
+        server_sock.listen(1)
+
+        while True:
+            client_sock, client_addr = server_sock.accept()
+            name = client_sock.recv(1024)
+            name = name.decode()
+
+            with clients_lock:
+                while name in clients:
+                    name += '1'
+                client_sock.sendall(bytes(str(len(name))+" ", 'utf-8'))   #odeslij poprawiony nick
+                client_sock.sendall(bytes(name, 'utf-8'))
+
+                addClient(name, client_sock)   # i tak musialoby zakladac locka na clients
+                                                # wiec i tak to wszystko by stalo
+                                                 # wiec nie ma po co robic tego w osobnym watku
+                clients.update({name: [client_sock, client_addr]})
+
+                client = threading.Thread(target=readClient, args=(name,))
+                client.deamon = True
+                client.start()
+
+                welcomeStr = "accepted\n"
+                client_sock.sendall(bytes("0", 'utf-8'))
+                client_sock.sendall(bytes(str(len(welcomeStr))+" ", 'utf-8'))
+                client_sock.sendall(bytes(welcomeStr, 'utf-8'))
+                print("accepted "+str(client_addr)+str(client_sock)+name)
+
+
+except:
+    traceback.print_exc()
+    close = True
---
+++ server.py
@@ -0,0 +1,270 @@
+import socket
+import time
+from tkinter import *
+import threading
+import queue
+from tkinter import messagebox
+import sys
+from signal import signal, SIGINT
+
+HOST = 'localhost'
+PORT = 50007
+name = ""
+
+send_lock = threading.Lock()
+
+def handler(signal_received, frame):
+    print("SIGINT")
+    try:
+        client.gui.windowObj.exit()
+    except:
+        pass
+    client_sock.close()
+    exit(0)
+
+
+class windowObj:
+    def __init__(self, window, controller):
+        self.controller = controller
+        self.window = window
+        self.window.title("Chat:"+name)
+        self.window.geometry('1000x600')
+
+        def exit(quitGui=True):
+            try:
+                client_sock.sendall(bytes("0 ", 'utf-8')) #nie mozna miec wiadomosci o dlugosci 0 wiec jak taka wyslemy to znaczy ze sie chcemy odpiac
+            except:
+                pass
+            self.controller.endApplication()
+            if quitGui:
+                self.window.destroy()
+
+
+        self.exitButton = Button(self.window, text="Exit", padx=70, pady=20, command=exit)
+        self.exitButton.grid(column=1, row=2)
+
+        self.exit = exit
+
+        self.entry = Text(self.window, width=70, height=10)
+        self.entry.grid(column=0, row=1)
+
+        self.listBox = Listbox(self.window, selectmode=SINGLE)
+        self.listBox.grid(column=1, row=0, ipadx=30, ipady=153, rowspan=2)
+        self.listBox.insert(1, "ALL")
+        self.listBox.activate(0)
+
+        self.textBox = Text(self.window, width=70, height=15, state=DISABLED)
+        self.textBox.grid(column=0, row=0)
+
+        def sendMsg(event=None):
+            text = self.entry.get("1.0", "end")
+            if len(text) == 0 or text == "" or text == '\n':
+                messagebox.showerror("Error", "Message must be non empty ://")
+                self.entry.delete("1.0", "end")
+                return 'break'
+            if not self.listBox.curselection():
+                messagebox.showerror("Error", "To whom?")
+                return 'break'
+            msg = self.listBox.get(self.listBox.curselection())+"\n"+text
+            size = len(msg)
+          #  print(size)  print(msg)
+            try:
+                with send_lock:
+                    client_sock.sendall(bytes(str(size)+" ", 'utf-8'))
+                    client_sock.sendall(bytes(msg, 'utf-8'))
+                    self.entry.delete("1.0", "end")
+            except:
+                messagebox.showerror("Error", "Connection lost")
+                self.msgButton.config(state="disabled")
+                self.entry.unbind('<Return>')
+            return 'break'
+
+        self.sendMsg = sendMsg
+
+        self.msgButton = Button(self.window, text="Send Message", padx=250, pady=20, command=sendMsg)
+        self.msgButton.grid(column=0, row=2)
+
+        self.entry.bind('<Return>', sendMsg)
+
+
+class GuiPart:
+    def __init__(self, window, queue,  controller):
+        self.controller = controller
+        self.queue = queue
+        self.windowObj = windowObj(window, controller)
+
+    def handleQueue(self):
+        while self.queue.qsize()>1: # musi byc i typ i msg
+            try:
+                type = self.queue.get(0)
+                msg = self.queue.get(0)
+                print("type: "+type+" "+msg)
+                if type == '1':    #mates add
+                    self.windowObj.listBox.insert(END, msg)
+                    print("add "+msg)
+                elif type == '2':   #mates remove
+                    idx = self.windowObj.listBox.get(0, END).index(msg)
+                    self.windowObj.listBox.delete(idx)
+                elif type == '0':
+                    self.windowObj.textBox.config(state="normal")
+                    self.windowObj.textBox.insert(END, msg)
+                    self.windowObj.textBox.config(state="disabled")
+                    print("msg "+msg)
+
+            except queue.Empty:
+                pass
+
+
+class ThreadedClient:
+    def __init__(self, master):
+        self.master = master
+        self.queue = queue.Queue()
+
+        self.running = 1
+        self.recvThread = threading.Thread(target=self.receive)
+        self.recvThread.daemon = True
+        self.recvThread.start()
+
+        self.connThread = threading.Thread(target=self.connectionCheck)
+        self.connThread.daemon = True
+        self.connThread.start()
+
+        self.error = ""
+
+        self.gui = GuiPart(master, self.queue, self)
+
+        self.callHandleQueue()
+
+    def callHandleQueue(self):
+        if self.error == "error":
+            self.gui.windowObj.msgButton.config(state="disabled")
+            self.gui.windowObj.entry.unbind('<Return>')
+            messagebox.showerror("Error", "Connection lost")
+            self.error = ""
+        self.gui.handleQueue()
+        if not self.running:
+            client_sock.close()
+            sys.exit(0)
+        self.master.after(200, self.callHandleQueue)
+
+    def connectionCheck(self):
+        while True:
+           # print("connection check")
+            with send_lock:
+                try:
+                    client_sock.sendall(bytes("c", 'utf-8'))
+                except:
+                    self.gui.windowObj.exit()   #server zerwal polaczenie
+            time.sleep(1)
+
+    def receive(self):
+        #odbierz wiadomosci
+        #inserty dla listBox
+        client_sock.settimeout(4.0) #czeka dluzej wiec server na pewno zdazy
+        while self.running:
+            try:
+                type = client_sock.recv(1).decode()
+            except socket.timeout:
+                self.error = "error"
+                return
+
+            if type == "3": #tylko sprawdzalismy czy jeszcze zyje
+                continue
+
+            self.queue.put(type)
+
+            size=""
+            temp=""
+            while not temp == " ":
+                size += temp
+                temp = client_sock.recv(1)
+                temp = temp.decode()
+
+            size = int(size)
+
+            data = client_sock.recv(size)
+            msg = data.decode()
+            self.queue.put(msg)
+            print(msg)
+            print(self.queue.qsize())
+        # dla kazdego mamy na kolejce type, msg
+
+
+    def endApplication(self):
+        self.running = 0
+
+
+#login:
+########################################################
+loginWindow = Tk()
+loginWindow.title("Login")
+loginWindow.geometry('230x90')
+enterName = Label(loginWindow, text="Enter nickname").grid(row=0)
+
+login = Entry(loginWindow)
+login.grid(row=1)
+login.focus_set()
+
+def ok(event=None):
+    if login.get() == "":
+        login.select_clear()
+        messagebox.showerror("Error", "Nickaname must be non empty")
+        return
+    if login.get() == 'ALL':
+        login.select_clear()
+        messagebox.showerror("Error", "Nickaname can't spell \"ALL\"")
+        return
+    if login.get().__contains__("\n"):
+        login.select_clear()
+        messagebox.showerror("Error", "Nickaname cannot contain \"\\n\"")
+        return
+    if len(login.get())>1000:
+        login.select_clear()
+        messagebox.showerror("Error", "Nickaname too long")
+        return
+    global name
+    name = login.get()
+    loginWindow.destroy()
+
+okButton = Button(loginWindow, text="OK", command=ok).grid(row=2)
+
+loginWindow.bind('<Return>', ok)
+
+loginWindow.mainloop()
+#########################################################
+
+if name == "":
+    sys.exit(0)
+
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_sock:
+    try:
+        client_sock.connect((HOST, PORT))
+        client_sock.sendall(bytes(name, 'utf-8'))
+        size = ""
+        temp = ""
+        while not temp == " ":
+            size += temp
+            temp = client_sock.recv(1)
+            temp = temp.decode()
+
+        size = int(size)
+        name = client_sock.recv(size).decode()
+        print(name)
+    except:
+        print("could not connect")
+        sys.exit(1)
+
+    signal(SIGINT, handler)
+
+    root = Tk()
+    client = ThreadedClient(root)
+    root.mainloop()
+
+
+    try:
+        client.gui.windowObj.exit()
+    except:
+        pass
+
